126cd2a60fb3607f0e62ec67fb92b33cf47b7220
ashwinpra <ashprasanth2003@gmail.com>
2022-05-25 20:42:57 +0530

    Rearranged js files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 // make contact form responsive
+
+
+// making sticky navbar
+window.addEventListener( "resize", ResizeWindow );
+function ResizeWindow( )
+{
+	var dataSpyList = [].slice.call( document.querySelectorAll( '[data-spy="scroll"]' ) );
+	
+	dataSpyList.forEach( function ( dataSpyElement )
+	{
+		bootstrap.ScrollSpy.getInstance( dataSpyElement ).refresh( );
+	} );
+}
+// end of sticky navbar
